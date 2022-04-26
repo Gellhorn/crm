5b13dc8e1c9b84194eac458430627c279711eca4
neil <1527656428@qq.com>
2022-04-26 21:49:24 +0800

    sale chance  查询

--- src/main/java/com/xxxx/crm/vo/SaleChance.java
+++ src/main/java/com/xxxx/crm/vo/SaleChance.java
@@ -1,5 +1,7 @@
 package com.xxxx.crm.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class SaleChance {
@@ -22,6 +24,7 @@ public class SaleChance {
     private String createMan;
 
     private String assignMan;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 
     private Date assignTime;
 
@@ -30,8 +33,9 @@ public class SaleChance {
     private Integer devResult;
 
     private Integer isValid;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 
     private Date updateDate;
 
