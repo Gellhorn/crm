8c8a4aeec36d1716bf2f1b4aab4feacff136f4eb
neil <1527656428@qq.com>
2022-04-27 22:05:10 +0800

    sale_chance  add 和update 后端接口 完成

--- src/main/java/com/xxxx/crm/controller/SaleChanceController.java
+++ src/main/java/com/xxxx/crm/controller/SaleChanceController.java
@@ -1,10 +1,16 @@
 package com.xxxx.crm.controller;
 
 import com.xxxx.crm.base.BaseController;
+import com.xxxx.crm.base.ResultInfo;
 import com.xxxx.crm.query.SaleChanceQuery;
 import com.xxxx.crm.service.SaleChanceService;
+import com.xxxx.crm.service.UserService;
+import com.xxxx.crm.utils.CookieUtil;
 import com.xxxx.crm.utils.LoginUserUtil;
+import com.xxxx.crm.vo.SaleChance;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,6 +25,9 @@ public class SaleChanceController extends BaseController {
     @Resource
     private SaleChanceService saleChanceService;
 
+    @Resource
+    private UserService userService;
+
     @RequestMapping("index")
     public String index(){
         return "saleChance/sale_chance";
@@ -27,8 +36,7 @@ public class SaleChanceController extends BaseController {
      * 营销机会数据查询（分页多条件查询） 101001
      *  如果flag的值不为空，且值为1，则表示当前查询的是客户开发计划；否则查询营销机会数据
      *
-     * 乐字节：专注线上IT培训
-     * 答疑老师微信：lezijie
+
      * @param saleChanceQuery
      * @return java.util.Map<java.lang.String,java.lang.Object>
      */
@@ -40,4 +48,48 @@ public class SaleChanceController extends BaseController {
 
         return saleChanceService.querySaleChanceByParams(saleChanceQuery);
     }
+    /**
+     * 添加营销机会 101002
+     * @param saleChance
+     * @return com.xxxx.crm.base.ResultInfo
+     */
+    @RequestMapping("add")
+    @ResponseBody
+    public ResultInfo addSaleChance(SaleChance saleChance, HttpServletRequest request) {
+
+        // 从Cookie中获取当前登录的用户名
+        String userName =  userService.selectByPrimaryKey(LoginUserUtil.releaseUserIdFromCookie(request)).getTrueName();
+
+        // 设置用户名到营销机会对象
+        saleChance.setCreateMan(userName);
+        // 调用Service层的添加方法
+        saleChanceService.addSaleChance(saleChance);
+        return success("营销机会数据添加成功！");
+    }
+    @RequestMapping("update")
+    @ResponseBody
+    public ResultInfo updateSaleChance(SaleChance saleChance) {
+        // 调用Service层的添加方法
+        saleChanceService.updateSaleChance(saleChance);
+        return success("营销机会数据更新成功！");
+    }
+    /**
+     * 进入添加/修改营销机会数据页面
+     * @param
+     * @return java.lang.String
+     */
+    @RequestMapping("addOrUpdateSaleChancePage")
+    public String toSaleChancePage(Integer saleChanceId, HttpServletRequest request) {
+
+        // 判断saleChanceId是否为空
+        if (saleChanceId != null) {
+            // 通过ID查询营销机会数据
+            SaleChance saleChance = saleChanceService.selectByPrimaryKey(saleChanceId);
+            // 将数据设置到请求域中
+            request.setAttribute("saleChance",saleChance);
+        }
+
+        return "saleChance/add_update";
+    }
+
  }
--- target/classes/static/js/saleChance/sale.chance.js
+++ target/classes/static/js/saleChance/sale.chance.js
@@ -81,97 +81,97 @@ layui.use(['table','layer'],function(){
         })
     });
 
-    // //头工具栏事件
-    // table.on('toolbar(saleChances)', function(obj){
-    //     var checkStatus = table.checkStatus(obj.config.id);
-    //     switch(obj.event){
-    //         case "add":
-    //             openAddOrUpdateSaleChanceDialog();
-    //             break;
-    //         case "del":
-    //             delSaleChance(checkStatus.data);
-    //             break;
-    //     };
-    // });
+    //头工具栏事件
+    table.on('toolbar(saleChances)', function(obj){
+        var checkStatus = table.checkStatus(obj.config.id);
+        switch(obj.event){
+            case "add":
+                openAddOrUpdateSaleChanceDialog();
+                break;
+            case "del":
+                delSaleChance(checkStatus.data);
+                break;
+        };
+    });
     //
     //
-    // /**
-    //  * 行监听
+    // // /**
+    // //  * 行监听
     //  */
-    // table.on("tool(saleChances)", function(obj){
-    //     var layEvent = obj.event;
-    //     if(layEvent === "edit") {
-    //         openAddOrUpdateSaleChanceDialog(obj.data.id);
-    //     }else if(layEvent === "del") {
-    //         layer.confirm('确定删除当前数据？', {icon: 3, title: "机会数据管理"}, function (index) {
-    //             $.post(ctx+"/sale_chance/delete",{ids:obj.data.id},function (data) {
-    //                 if(data.code==200){
-    //                     layer.msg("操作成功！");
-    //                     tableIns.reload();
-    //                 }else{
-    //                     layer.msg(data.msg, {icon: 5});
-    //                 }
-    //             });
-    //         })
-    //     }
-    // });
+    table.on("tool(saleChances)", function(obj){
+        var layEvent = obj.event;
+        if(layEvent === "edit") {
+            openAddOrUpdateSaleChanceDialog(obj.data.id);
+        }else if(layEvent === "del") {
+            layer.confirm('确定删除当前数据？', {icon: 3, title: "机会数据管理"}, function (index) {
+                $.post(ctx+"/sale_chance/delete",{ids:obj.data.id},function (data) {
+                    if(data.code==200){
+                        layer.msg("操作成功！");
+                        tableIns.reload();
+                    }else{
+                        layer.msg(data.msg, {icon: 5});
+                    }
+                });
+            })
+        }
+    });
     //
     //
-    // // 打开添加机会数据页面
-    // function openAddOrUpdateSaleChanceDialog(sid){
-    //     var url  =  ctx+"/sale_chance/addOrUpdateSaleChancePage";
-    //     var title="营销机会管理-机会添加";
-    //     if(sid){
-    //         url = url+"?id="+sid;
-    //         title="营销机会管理-机会更新";
-    //     }
-    //     layui.layer.open({
-    //         title : title,
-    //         type : 2,
-    //         area:["700px","560px"],
-    //         maxmin:true,
-    //         content : url
-    //     });
-    // }
-    //
-    //
-    // /**
-    //  * 批量删除
-    //  * @param datas
-    //  */
-    // function delSaleChance(datas) {
-    //     if(datas.length==0){
-    //         layer.msg("请选择删除记录!", {icon: 5});
-    //         return;
-    //     }
-    //     layer.confirm('确定删除选中的机会数据？', {
-    //         btn: ['确定','取消'] //按钮
-    //     }, function(index){
-    //         layer.close(index);
-    //         var ids= "ids=";
-    //         for(var i=0;i<datas.length;i++){
-    //             if(i<datas.length-1){
-    //                 ids=ids+datas[i].id+"&ids=";
-    //             }else {
-    //                 ids=ids+datas[i].id
-    //             }
-    //         }
-    //         $.ajax({
-    //             type:"post",
-    //             url:ctx+"/sale_chance/delete",
-    //             data:ids,
-    //             dataType:"json",
-    //             success:function (data) {
-    //                 if(data.code==200){
-    //                     tableIns.reload();
-    //                 }else{
-    //                     layer.msg(data.msg, {icon: 5});
-    //                 }
-    //             }
-    //         })
-    //     });
-    // }
-    //
+    // 打开添加机会数据页面
+    function openAddOrUpdateSaleChanceDialog(sid){
+        var url  =  ctx+"/sale_chance/addOrUpdateSaleChancePage";
+        var title="营销机会管理-机会添加";
+        if(sid){
+            url = url+"?id="+sid;
+            title="营销机会管理-机会更新";
+        }
+        layui.layer.open({
+            title : title,
+            type : 2,
+            area:["700px","560px"],
+            maxmin:true,
+            content : url
+        });
+    }
+
+
+    /**
+     * 批量删除
+     * @param datas
+     */
+    function delSaleChance(datas) {
+        if(datas.length==0){
+            layer.msg("请选择删除记录!", {icon: 5});
+            return;
+        }
+        layer.confirm('确定删除选中的机会数据？', {
+            btn: ['确定','取消'] //按钮
+        }, function(index){
+            layer.close(index);
+            var ids= "ids=";
+            for(var i=0;i<datas.length;i++){
+                if(i<datas.length-1){
+                    ids=ids+datas[i].id+"&ids=";
+                }else {
+                    ids=ids+datas[i].id
+                }
+            }
+            $.ajax({
+                type:"post",
+                url:ctx+"/sale_chance/delete",
+                data:ids,
+                dataType:"json",
+                success:function (data) {
+                    if(data.code==200){
+                        tableIns.reload();
+                    }else{
+                        layer.msg(data.msg, {icon: 5});
+                    }
+                }
+            })
+        });
+    }
+
 
 
 
